0e05822c7ee39cd64f0df1e49c1fdb5cd92854d7
bentopedro <63908808+bentopedro@users.noreply.github.com>
2022-05-13 02:52:31 +0100

    Adicionando tipos
    - criação da primeira rota

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import { createCourse } from './routes';
 
 const app = express();
 
-app.get('/', (request, response) =>{
-    return response.json({ 
-        message : "Hello world!"
-    })
-})
+
+app.get('/', createCourse)
+
 
 app.listen(3333, ()=> console.log("server runnig"));
+
+// yarn add typescript -D para instalar o TS.
+// yarn tsc --init  para inicializar o TS
+/*
+ o yarn tsc converte o file TS para JS para dentro da pasta dist se informar no file tsconfig.json o destino no outDir="./dist" 
+ se não, cria dentro do src mesmo
+*/
